036447eece5636883c973d4c2258a5509d1d2a81
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-18 21:27:26 +0600

    added preventdefalut

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('login-btn').addEventListener('click', function(e){
+    e.preventDefault()
+   
+    const phoneInput = document.getElementById('phone-num').value;
+    console.log(phoneInput)
+})
