4ebb782e027559cde3dca700ec303cf826c0a55e
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-09-18 22:05:28 +0600

    pin match

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-document.getElementById('login-btn').addEventListener('click', function(e){
-    e.preventDefault()
-   
-    const phoneInput = document.getElementById('phone-num').value;
-    console.log(phoneInput)
+
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault()
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    // console.log(phoneNumber, pinNumber)
+
+    if ( phoneNumber === '5' && pinNumber === '1234'){
+        window.location.href = '/home.html'
+    }else{
+        alert('Enter your correct pin or number')
+        document.getElementById('phone-number').value = ''
+        document.getElementById('pin-number').value = ''
+    }
+
 })
